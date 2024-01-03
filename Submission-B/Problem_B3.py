98327aaac294ec80174b21322cf2b429832de2d9
Mayori Permitha <141924190+haomail@users.noreply.github.com>
2024-01-04 00:37:31 +0700

    Create Problem_B3.py

---
+++ Submission-B/Problem_B3.py
@@ -0,0 +1,95 @@
+# ========================================================================================
+# PROBLEM B3
+#
+# Build a CNN based classifier for Rock-Paper-Scissors dataset.
+# Your input layer should accept 150x150 with 3 bytes color as the input shape.
+# This is unlabeled data, use ImageDataGenerator to automatically label it.
+# Don't use lambda layers in your model.
+#
+# The dataset used in this problem is created by Laurence Moroney (laurencemoroney.com).
+#
+# Desired accuracy AND validation_accuracy > 83%
+# ========================================================================================
+
+import urllib.request
+import zipfile
+import tensorflow as tf
+import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop
+
+
+def solution_B3():
+    data_url = 'https://github.com/dicodingacademy/assets/releases/download/release-rps/rps.zip'
+    urllib.request.urlretrieve(data_url, 'rps.zip')
+    local_file = 'rps.zip'
+    zip_ref = zipfile.ZipFile(local_file, 'r')
+    zip_ref.extractall('data/')
+    zip_ref.close()
+
+    TRAINING_DIR = "data/rps/"
+    training_datagen = ImageDataGenerator(rescale=1. / 255.,
+                                          rotation_range=30,
+                                          width_shift_range=0.2,
+                                          height_shift_range=0.2,
+                                          shear_range=0.1,
+                                          zoom_range=0.1,
+                                          horizontal_flip=True,
+                                          fill_mode='nearest')
+
+    # YOUR IMAGE SIZE SHOULD BE 150x150
+    # Make sure you used "categorical"
+    train_generator = training_datagen.flow_from_directory(directory=TRAINING_DIR,
+                                                           batch_size=50,
+                                                           class_mode='categorical',
+                                                           target_size=(150, 150))
+
+    VALIDATION_DIR = "data/rps/"
+    validation_datagen = ImageDataGenerator(rescale=1. / 255)
+    validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
+                                                                  class_mode='categorical',
+                                                                  batch_size=32,
+                                                                  target_size=(150, 150))
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        # YOUR CODE HERE, end with 3 Neuron Dense, activated by softmax
+        tf.keras.layers.Dense(3, activation='softmax')
+    ])
+
+    # Compile the model
+    model.compile(optimizer=RMSprop(learning_rate=0.001),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # Define a callback to stop training when when both accuracy and validation accuracy exceed 97%
+    class Callback(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs={}):
+            if (logs.get('accuracy') > 0.83 and logs.get('val_accuracy') > 0.83):
+                print("\nAccuracy and Validation Accuracy is more than 83%!")
+                self.model.stop_training = True
+
+    # Instantiate the callback
+    acc_callback = Callback()
+
+    # Train the model
+    model.fit(train_generator,
+              validation_data=validation_generator,
+              epochs=40,
+              callbacks=[acc_callback])
+    return model
+
+
+# The code below is to save your model as a .h5 file.
+# It will be saved automatically in your Submission folder.
+if __name__ == '__main__':
+    # DO NOT CHANGE THIS CODE
+    model = solution_B3()
+    model.save("model_B3.h5")
